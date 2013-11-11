19a91256dff1f6bcd26429dd5d67f6c01c5833fb
Susanoo <susanoo@SusanoodeMacBook-Air.local>
2013-11-11 20:25:21 +0800

    Initial Commit

---
+++ sWeiBo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sWeiBo
+//
+//  Created by Susanoo on 13-11-11.
+//  Copyright (c) 2013年 Susanoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
